4de2c111c42787e55042e4473a4b175740977587
aryan-cs <aryan05g@gmail.com>
2023-01-29 20:09:53 -0600

    initial commit

--- App.js
+++ App.js
@@ -2,19 +2,38 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <Text>Welcome to</Text>
+      <Text style = {styles.title}>Red Line.</Text>
+      <StatusBar style = "auto" />
+
     </View>
+
   );
+
 }
 
 const styles = StyleSheet.create({
+
   container: {
+
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
+
   },
+
+  title: {
+
+    fontSize: 40,
+    fontWeight: 'bold',
+    color: 'red'
+
+  }
+
 });
