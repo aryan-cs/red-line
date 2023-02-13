656e4b40ee6ddca086e7829119aa0d8b6abefb61
aryan <64030782+aryan-cs@users.noreply.github.com>
2023-02-13 20:31:13 +0000

    added font

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
 
 export default function App() {
 
+  const [fontsLoaded] = useFonts({
+
+    'Montserrat-Italic': require('./assets/fonts/montserrat-italic.ttf'),
+    'Montserrat-SemiBold-Italic': require('./assets/fonts/montserrat-semibold-italic.ttf'),
+    'Montserrat-Regular': require('./assets/fonts/montserrat.ttf'),
+
+  });
+
   return (
 
-    <View style={styles.container}>
+    <View style = {styles.container}>
 
       <Text>Welcome to</Text>
-      <Text style = {styles.title}>Red Line.</Text>
+      <Text style = {styles.title}>RED LINE</Text>
       <StatusBar style = "auto" />
 
     </View>
@@ -24,15 +33,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
 
   },
 
   title: {
 
-    fontSize: 40,
-    fontWeight: 'bold',
-    color: 'red'
+    fontSize: 70,
+    color: 'red',
+    fontFamily: 'Montserrat-SemiBold-Italic',    
 
   }
 
