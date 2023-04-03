a8a732ec7936a74b4fc3c8d6b331d145ec167229
aryan-cs <aryan05g@gmail.com>
2023-04-02 21:41:01 -0500

    back to school :(

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -44,14 +44,6 @@ export default function ({ navigation }) {
           ...allFeed,
         ]);
 
-        // console.log("---------------------------");
-        // console.log("Post: " + post.title);
-        // console.log("Caption: " + post.caption);
-        // console.log("Description: " + post.description);
-        // console.log("Image: " + post.imagePath);
-        // console.log("Timestamp: " + post.timestamp);
-        // console.log("---------------------------");
-
       });
 
     });
@@ -62,56 +54,56 @@ export default function ({ navigation }) {
 
     <View style = {{ flex: 1, backgroundColor: isDarkmode ? VARS.darkmodeBG : VARS.lightmodeBG }}>
 
-    <View style = {{
-						
-      marginTop: -60,
-      paddingTop: 60,
-      backgroundColor: isDarkmode ? VARS.darkmodeBGdarker : VARS.lightmodeBG,
-                
-    }}>
-
-<TouchableOpacity
-						style = {{
-							width: 60,
-							height: 60,
-							position: "absolute",
-              bottom: "2%",
-              right: "3.5%",
-              justifyContent: "center",
-              alignItems: "center",
-              textAlign: "center",
-							backgroundColor: VARS.redline,
-              borderRadius: "100%",
-							zIndex: 1,
-						}}
-						onPress = {() => { alert("Feature coming soon!"); }}>
-							
-						<Ionicons
-							name = {"ios-add"}
-							style = {{marginLeft: 2, marginTop: 1}}
-							size = {40}
-							color = { themeColor.white100 }/>
-
-					</TouchableOpacity>
-
-    <ScrollView contentContainerStyle = {{ flexGrow: 1 }}>
-
       <View style = {{
-        alignItems: "center",
-        justifyContent: "center",
-        marginHorizontal: 20,
-        marginTop: 60,
-        marginBottom: 60,
-        zIndex: 1
+              
+        marginTop: -60,
+        paddingTop: 60,
+        backgroundColor: isDarkmode ? VARS.darkmodeBGdarker : VARS.lightmodeBG,
+                  
       }}>
 
-        {allFeed}
+        <TouchableOpacity
+          style = {{
+            width: 60,
+            height: 60,
+            position: "absolute",
+            bottom: "2%",
+            right: "3.5%",
+            justifyContent: "center",
+            alignItems: "center",
+            textAlign: "center",
+            backgroundColor: VARS.redline,
+            borderRadius: "100%",
+            zIndex: 1,
+          }}
+          onPress = {() => { alert("Feature coming soon!"); }}>
+            
+          <Ionicons
+            name = {"ios-add"}
+            style = {{marginLeft: 2, marginTop: 1}}
+            size = {40}
+            color = { themeColor.white100 }/>
+
+        </TouchableOpacity>
+
+        <ScrollView contentContainerStyle = {{ flexGrow: 1 }}>
+
+          <View style = {{
+            alignItems: "center",
+            justifyContent: "center",
+            marginHorizontal: 20,
+            marginTop: 60,
+            marginBottom: 60,
+            zIndex: 1
+          }}>
+
+            {allFeed}
+
+          </View>
+
+        </ScrollView>
 
       </View>
-
-    </ScrollView>
-
-    </View>
 
     </View>
 
